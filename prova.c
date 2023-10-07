0e4b007fe857ba9a65e83bde00ac96cae1ea0e81
LAPTOP-UMEVT07V\hp_Vincio <Vvinciguerra6@gmail.com>
2023-10-07 22:46:30 +0200

    Terza Modifica.

--- prova.c
+++ prova.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "struct.h"
 
 void main(){
-    printf("cioa come stai?\n");
-    printf("sarei cotento di riuscire a commuttare senza problemi?\n");
+    
+    albero* a = (albero*)malloc(sizeof(albero));
+    albero* right_son= (albero*)malloc(sizeof(albero));
+    albero* left_son = (albero*)malloc(sizeof(albero));
+    a->info=0;
+    a->left=left_son;
+    a->right=right_son;
+    
+    printf("il numero dei nodi e' : %d\n",conta_nodi(a));
+    printf("l'altezza dell'albero e' : %d\n",altezza(a));
+
 }
+
+
